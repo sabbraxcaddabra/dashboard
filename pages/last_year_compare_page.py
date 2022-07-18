d5be94e18294ee26057b6e47b6b0c3d27e90ea7b
sabbraxcaddabra <sabbraxcaddabra@yandex.ru>
2022-07-19 00:14:56 +0300

    Доделываем потихоньку сравнение с прошлым годом

--- pages/last_year_compare_page.py
+++ pages/last_year_compare_page.py
@@ -61,7 +61,7 @@ def get_df_by_date(df, date):
 def get_df_by_del_date(df, date):
     date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
 
-    df = df[(df['add_data'] <= date) & (df['del_data'] <= date)]
+    df = df[(df['add_data'] <= date) & (df['del_data'] >= date)]
 
     return df
 
@@ -86,7 +86,7 @@ control_elements = html.Div(children=[
         dbc.Col([
             html.Div('Выбор даты'),
             dcc.DatePickerSingle(  # Выбор промежутка дат
-                id='pick_a_date',
+                id='pick_a_date_single',
                 date=DATA_LOADER.data['add_data'].max(),
                 max_date_allowed=DATA_LOADER.data['add_data'].max(),
                 min_date_allowed=DATA_LOADER.data['add_data'].min(),
@@ -111,7 +111,13 @@ control_elements = html.Div(children=[
 ])
 
 layout = html.Div(children=[
+    dcc.Interval(id='update_interval', interval=300e3),
+    html.Div(id='total_table', style={'marginRight': 700}),
+    html.Br(),
     control_elements,
+    html.Br(),
+    html.Div(id='total_budget', style={'fontSize': '22px'}),
+    html.Br(),
     dbc.Row(children=[
         dbc.Col(dcc.Graph(id='compare_adm_cond_osn_kontract')), # Сравнение двух лет по условиям поступления
         dbc.Col(dcc.Graph(id='compare_adm_cond_os_celo')), # Сравнение двух лет по условиям поступления
@@ -155,13 +161,8 @@ def get_counts_and_people_counts(df):
 
     return counts, people_counts
 
-def get_load_figure(
-        counts,
-        people_counts,
-        color,
-        people_color,
-        fig_type='not_cum'):
 
+def get_load_scater(counts, people_counts, fig_type='not_cum'):
     locate_date = {  # Переименование месяца в дате
         '03': 'Март',
         '04': 'Апрель',
@@ -184,7 +185,6 @@ def get_load_figure(
         values = counts.values
         title_text_yaxis = 'Число заявлений / людей'
 
-
     new_date = []
     new_date_people = []
 
@@ -198,13 +198,29 @@ def get_load_figure(
         new_dat[0] = locate_date[new_dat[0]]
         new_date_people.append('-'.join(new_dat))
 
+    return values, people_values, new_date, new_date_people
 
+
+def get_load_figure(
+        counts,
+        people_counts,
+        color,
+        people_color,
+        fig_type='not_cum'):
+
+    values, people_values, new_date, new_date_people = get_load_scater(counts, people_counts, fig_type)
     fig = go.Figure()
     fig.add_traces(data=[
         go.Scatter(x=new_date, y=values, name='Заявления', mode='none', fill='tozeroy', fillcolor=color),
         go.Scatter(x=new_date_people, y=people_values, name='Люди', fill='tozeroy', fillcolor=people_color)
     ]
     )
+
+    if fig_type == 'cum':
+        title_text_yaxis = 'Суммарное число заявлений / людей'
+    else:
+        title_text_yaxis = 'Число заявлений / людей'
+
     fig.update_xaxes(title_text='Дата')
     fig.update_yaxes(title_text=title_text_yaxis)
 
@@ -215,7 +231,7 @@ def get_load_figure(
 
 @callback(
     [Output('daily_load_plot_not_cum', 'figure'), Output('daily_load_plot_cum', 'figure')],
-    [Input('pick_a_date', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value'),
+    [Input('pick_a_date_single', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value'),
      Input('type_f_dropdown', 'value'), Input('post_method_dropdown', 'value')
      ]
 )
@@ -223,19 +239,105 @@ def plot_daily_load(date, edu_level, edu_form, fintype, post_method):
     df = DATA_LOADER.data
     df = filter_all(df, date, edu_level, edu_form, fintype, post_method)
 
+    df_21 = DATA_LOADER.last_year_df
+    df_21 = filter_all(df_21, date, edu_level, edu_form, fintype, post_method)
+    df_21 = get_df_by_del_date(df_21, date)
+
     counts, people_counts = get_counts_and_people_counts(df)
+    counts_21, people_counts_21 = get_counts_and_people_counts(df_21)
+
+    total_counts = pd.DataFrame(counts).join(counts_21, rsuffix='_2021').fillna(0)
+    total_people_counts = pd.DataFrame(people_counts).join(people_counts_21, rsuffix='_2021').fillna(0)
+
+    counts = total_counts['add_data_m_d']
+    people_counts = total_people_counts['add_data_m_d']
 
     fig = get_load_figure(counts, people_counts, '#00CC00', '#FF8500','not_cum')
     fig_cum = get_load_figure(counts, people_counts, '#0776A0', '#FF8500', 'cum')
 
+    counts = total_counts['add_data_m_d_2021']
+    people_counts = total_people_counts['add_data_m_d_2021']
+
+    values, people_values, new_date, new_date_people = get_load_scater(counts, people_counts, fig_type='not_cum')
+
+    fig.add_traces([
+        go.Scatter(x=new_date, y=values, name='Заявления 2021', mode='lines+markers', line_color='#F23911'),
+        go.Scatter(x=new_date_people, y=people_values, name='Люди 2021', mode='lines+markers')
+    ])
+
+    values, people_values, new_date, new_date_people = get_load_scater(counts, people_counts, fig_type='cum')
+
+    fig_cum.add_traces([
+        go.Scatter(x=new_date, y=values, name='Заявления 2021', mode='lines+markers', line_color='#F23911'),
+        go.Scatter(x=new_date_people, y=people_values, name='Люди 2021', mode='lines+markers')
+    ])
+
+
     return fig, fig_cum
 
-@callback(
-    [Output('compare_adm_cond_osn_kontract', 'figure'), Output('compare_adm_cond_os_celo', 'figure')],
-    [Input('pick_a_date', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value')]
-)
-def plot_compare_adm_plot(date, edu_level, edu_form):
+def get_stats_by_year(df):
 
+    res_dict = dict(
+        n_abiturients=df.drop_duplicates('abiturient_id').shape[0],
+        n_budget=df[df['fintype'] != 'С оплатой обучения'].shape[0],
+        n_dou=df[df['fintype'] == 'С оплатой обучения'].shape[0],
+        n_application=df.shape[0]
+    )
+
+    return res_dict
+
+@callback(
+    Output('pick_a_date_single', 'max_date_allowed'),
+    [Input('update_interval', 'n_intervals')]
+)
+def update_dates_range(n):
+    df = DATA_LOADER.data
+    max_date = df['add_data'].max()
+    return max_date
+
+@callback(
+    Output('total_table', 'children'),
+    [Input('update_interval', 'n_intervals'), Input('pick_a_date_single', 'date')]
+)
+def update_table(n_interval, date):
+    DATA_LOADER.load_data()
+
+    df = DATA_LOADER.data
+    df = get_ok_status(df)
+    df = get_df_by_date(df, date)
+
+    df_21 = DATA_LOADER.last_year_df
+    df_21 = get_df_by_del_date(df_21, date)
+
+    dict_22 = get_stats_by_year(df)
+    dict_21 = get_stats_by_year(df_21)
+
+    compare_df = pd.DataFrame(
+        data={
+            '': ['Абитуриенты, чел.', 'Бюджет, заяв.', 'ДОУ, заяв.', 'Итог, заяв.'],
+            '2021': [dict_21['n_abiturients'], dict_21['n_budget'], dict_21['n_dou'], dict_21['n_application']],
+            '2022': [dict_22['n_abiturients'], dict_22['n_budget'], dict_22['n_dou'], dict_22['n_application']]
+        }
+    )
+
+    d_table  = dash.dash_table.DataTable(
+        data=compare_df.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in compare_df.columns],
+        style_cell={'font_size': '20px',
+                    'text_align': 'center'
+                    },
+    )
+
+    return d_table
+
+@callback(
+    [Output('compare_adm_cond_osn_kontract', 'figure'),
+     Output('compare_adm_cond_os_celo', 'figure'),
+     Output('total_budget', 'children')
+     ],
+    [Input('update_interval', 'n_intervals'), Input('pick_a_date_single', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value')]
+)
+def plot_compare_adm_plot(n_intervals, date, edu_level, edu_form):
 
     df = DATA_LOADER.data
     df = get_ok_status(df)
@@ -251,8 +353,8 @@ def plot_compare_adm_plot(date, edu_level, edu_form):
     df = get_only_needed_cols(df)
     df_21 = get_only_needed_cols(df_21)
 
-    df['year'] = 2022
-    df_21['year'] = 2021
+    df['Год'] = 2022
+    df_21['Год'] = 2021
 
     total_df = pd.concat((df, df_21), ignore_index=True)
 
@@ -263,8 +365,8 @@ def plot_compare_adm_plot(date, edu_level, edu_form):
     os_and_celo = total_df[(total_df['fintype'] == 'Особая квота') | (total_df['fintype'] == 'Целевая квота')]
     osn_and_kontract = total_df[(total_df['fintype'] == 'Основные места') | (total_df['fintype'] == 'С оплатой обучения')]
 
-    fig_os_and_celo = px.histogram(os_and_celo, x='fintype', color='year', barmode='group', text_auto=True)
-    fig_osn_and_kontract = px.histogram(osn_and_kontract, x='fintype', color='year', barmode='group', text_auto=True)
+    fig_os_and_celo = px.histogram(os_and_celo, x='fintype', color='Год', barmode='group', text_auto=True)
+    fig_osn_and_kontract = px.histogram(osn_and_kontract, x='fintype', color='Год', barmode='group', text_auto=True)
 
     fig_osn_and_kontract.update_traces(textposition='outside')
     fig_os_and_celo.update_traces(textposition='outside')
@@ -276,8 +378,7 @@ def plot_compare_adm_plot(date, edu_level, edu_form):
         xaxis_title="Тип финансирования",
         yaxis_title="Кол-во заявлений",
         font=dict(
-            family="Courier New, monospace",
-            size=18,
+            size=18
         )
     )
 
@@ -286,7 +387,6 @@ def plot_compare_adm_plot(date, edu_level, edu_form):
         xaxis_title="Тип финансирования",
         yaxis_title="Кол-во заявлений",
         font=dict(
-            family="Courier New, monospace",
             size=18,
         )
     )
@@ -294,31 +394,14 @@ def plot_compare_adm_plot(date, edu_level, edu_form):
     fig_osn_and_kontract.update_yaxes(range=[0, max_osn_kontract * 1.2])
     fig_os_and_celo.update_yaxes(range=[0, max_os_celo * 1.2])
 
-    return fig_osn_and_kontract, fig_os_and_celo
+    div_text = f'Всего заявлений на бюджет (21/22): {df.shape[0]}/{df_21.shape[0]}'
 
-    # fig.add_trace(go.Bar(name="Основные места",
-    #                      x=["2022", "2021"],
-    #                      y=[counts.get("Основные места", 0), counts_21.get("Основные места", 0)]), row=1, col=1)
-    # fig.add_trace(go.Bar(name="С оплатой обучения",
-    #                      x=["2022", "2021"],
-    #                      y=[counts.get("С оплатой обучения", 0), counts_21.get("С оплатой обучения", 0)]), row=1, col=2)
-    # fig.add_trace(go.Bar(name="Особая квота",
-    #                      x=["2022", "2021"],
-    #                      y=[counts.get("Особая квота", 0), counts_21.get("Особая квота", 0)]), row=2, col=1)
-    # fig.add_trace(go.Bar(name="Целевая квота",
-    #                      x=["2022", "2021"],
-    #                      y=[counts.get("Целевая квота", 0), counts_21.get("Целевая квота", 0)]), row=2, col=2)
-    #
-    # # fig.update_layout(
-    # #     xaxis_title="Тип финансирования",
-    # #     yaxis_title="Кол-во заявлений"
-    # # )
-    # #
-    # return fig
+    return fig_osn_and_kontract, fig_os_and_celo, div_text
+
 
 @callback(
     Output('compare_adm_cond_speces', 'figure'),
-    [Input('pick_a_date', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value'),
+    [Input('pick_a_date_single', 'date'), Input('edu_level_dropdown', 'value'), Input('edu_form', 'value'),
      Input('type_f_dropdown', 'value'), Input('post_method_dropdown', 'value')
      ]
 )
@@ -336,11 +419,11 @@ def plot_compare_adm_plot(date, edu_level, edu_form, fintype, post_method):
     df = get_only_needed_cols(df)
     df_21 = get_only_needed_cols(df_21)
 
-    df['year'] = 2022
-    df_21['year'] = 2021
+    df['Год'] = 2022
+    df_21['Год'] = 2021
 
     total_df = pd.concat((df, df_21), ignore_index=True)
-    fig = px.histogram(total_df, x='spec_code', color='year', barmode='group').update_xaxes(categoryorder='total ascending')
+    fig = px.histogram(total_df, x='spec_code', color='Год', barmode='group').update_xaxes(categoryorder='total ascending')
 
     fig.update_layout(
         xaxis_title="Код направления подготовки",
